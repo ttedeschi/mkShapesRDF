b27f7193158109c8a1b19c0d2921279c4763c53e
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2023-04-28 11:08:54 +0200

    Fix environment and dependecies

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # LCG_102
-. /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+. /cvmfs/sft.cern.ch/lcg/views/LCG_103/x86_64-centos7-gcc11-opt/setup.sh
 . myenv/bin/activate
