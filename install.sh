c9bef1607983ca3663d77e852f32fcb0d5c1f223
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2023-05-17 02:12:46 +0200

    small install fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #sourceCommand="source /root/.bashrc && mamba activate cern"
 sourceCommand="source /cvmfs/sft.cern.ch/lcg/views/LCG_103/x86_64-centos7-gcc11-opt/setup.sh"
 
-bash -c "$sourceCommand"
+eval "$sourceCommand"
 python -m venv --system-site-packages myenv
 source myenv/bin/activate
 
